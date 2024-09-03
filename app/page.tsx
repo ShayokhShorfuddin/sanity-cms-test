0d49a70115418365e1b8ab55715571e3f5a73849
Shayokh <shayokhshuvro14@gmail.com>
2024-09-03 19:09:55 +0600

    edit Project type placement and update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import { getProjects } from "@/sanity/sanity.utils";
+import { Project } from "@/types/Project";
+import Image from "next/image";
 
 export default async function Home() {
   const projects = await getProjects();
@@ -7,9 +9,15 @@ export default async function Home() {
     <>
       <h1>My projects go here!</h1>
 
-      {projects.map((project: any) => (
+      {projects.map((project) => (
         <div key={project._id}>
           <h1> {project.name} </h1>
+          <Image
+            src={project.image}
+            alt={project.name}
+            width={100}
+            height={100}
+          />
         </div>
       ))}
     </>
