fc250a145d684e7ab41f4b8cdd9a2c0c1c7f1015
Shayokh <shayokhshuvro14@gmail.com>
2024-09-03 20:37:49 +0600

    add new util getProject, create new page for individual projects using slug.

---
+++ app/projects/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import { getProject } from "@/sanity/sanity.utils";
+import Image from "next/image";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function Project({ params }: Props) {
+  const slug = params.slug;
+  const project = await getProject(slug);
+
+  return (
+    <div>
+      <Image src={project.image} alt={project.name} width={500} />
+      <p> {project.name} </p>
+    </div>
+  );
+}
--- sanity/sanity.utils.ts
+++ sanity/sanity.utils.ts
@@ -18,3 +18,24 @@ export async function getProjects(): Promise<Project[]> {
     content
   }`);
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: "bqol8hk6",
+    dataset: "production",
+    apiVersion: "2024-09-03",
+  });
+
+  return client.fetch(
+    groq`*[_type == "project" && slug.current == $slug][0]{
+    _id,
+    _createdAt,
+    name,
+    "slug": slug.current,
+    "image": image.asset->url,
+    url,
+    content
+  }`,
+    { slug }
+  );
+}
